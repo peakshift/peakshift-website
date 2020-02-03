09d995ee845212869ae948a9c170b14e2d54af18
Kenroy George <kenroy@garakami.com>
2020-02-03 22:01:31 +0100

    chore(*): add signatures for team

---
+++ signature/index.php
@@ -0,0 +1,7 @@
+<div style="padding-top:1.5em;margin-top:1.5em;border-top:1px solid #EEE; height:44px;color:#141414;font-family:Helvetica,verdana,sans-serif;line-height:18px;font-size:12px;overflow:hidden;">
+	<img src="http://peakshift.com/signature/img.php" width="133" height="44" style="float:left;padding-right:24px;">
+	<div style="float:left;padding-top:4px;">
+	<b>Peak Shift Inc.</b><br/>
+	<span style="font-size:10px;text-transform:uppercase;color:#AAA;">Skype: <a style="text-decoration:none;color:#00aff0;" href="skype:peakshift?call">peakshift</a></span>
+	</div>
+</div>
