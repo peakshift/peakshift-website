c4e3f01d8cd4f99db180b7d7d2ebe532fc1f9b07
MTG\mtg09 <mtg.software.dev@gmail.com>
2023-06-01 14:33:23 +0400

    update: add animation to the research lab section, update favicon

--- v2/src/app/home/ResearchLab.tsx
+++ v2/src/app/home/ResearchLab.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import { CgFileDocument } from "react-icons/cg";
 import { FiChevronRight, FiUsers, FiEyeOff, FiSettings } from "react-icons/fi";
 import { AiOutlineScan, AiOutlineWallet, AiOutlineSwap } from "react-icons/ai";
@@ -8,73 +9,120 @@ import { condensedHeadings, serifText } from "@/assets/fonts";
 import BgImage from "@/assets/images/research-tool.svg";
 import Button from "@/Components/Button/Button";
 import Image from "next/image";
+import {
+  useScroll,
+  motion,
+  useMotionValueEvent,
+  useTransform,
+  MotionValue,
+} from "framer-motion";
 
 export default function ResearchLab() {
+  const container = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("Page scroll: ", latest);
+  });
+
   return (
-    <section className="min-h-[min(100vh,1200px)] flex justify-center relative py-80 isolate z-10">
-      <Image
-        src={BgImage}
-        alt=""
-        className="absolute inset-0 z-0 inset-y-0 left-0 -translate-x-1/2 max-h-full"
-      />
-      <Container>
-        <div className="flex justify-between gap-40 h-full w-full items-center relative">
-          <div className="max-w-[480px]">
-            <h2
-              className={`${condensedHeadings.className} text-primary text-[48px] font-light`}
-            >
-              RESEARCH LAB
-            </h2>
-            <p className={`${serifText.className} italic text-body1`}>
-              We mix technical knowledge with human centred design in order to
-              discover design patterns for bleeding edge technologies. in order
-              to make them more accessible to the mass market.
-            </p>
-            <Button className="mt-40">
-              <CgFileDocument /> Read our research <FiChevronRight />
-            </Button>
+    <section
+      className="min-h-[min(500vh,6000px)] relative py-80 isolate z-10"
+      ref={container}
+    >
+      <div className="sticky top-0">
+        <Image
+          src={BgImage}
+          alt=""
+          className="absolute inset-y-0 left-0 z-0 -translate-x-1/2 h-full"
+        />
+        <Container className="relative">
+          <div className="flex justify-between gap-40 min-h-[100vh] w-full items-center ">
+            <div className="max-w-[480px]">
+              <h2
+                className={`${condensedHeadings.className} text-primary text-[48px] font-light`}
+              >
+                RESEARCH LAB
+              </h2>
+              <p className={`${serifText.className} italic text-body1`}>
+                We mix technical knowledge with human centred design in order to
+                discover design patterns for bleeding edge technologies. in
+                order to make them more accessible to the mass market.
+              </p>
+              <Button className="mt-40">
+                <CgFileDocument /> Read our research <FiChevronRight />
+              </Button>
+            </div>
+            <div className="grid grid-cols-2 gap-32">
+              <ul className="flex flex-col gap-32">
+                {researches.slice(0, 3).map((research, i) => (
+                  <ResearchCard
+                    key={research.title}
+                    research={research}
+                    index={i}
+                    scrollYProgress={scrollYProgress}
+                  />
+                ))}
+              </ul>
+              <ul className="flex flex-col gap-32 mt-64">
+                {researches.slice(3).map((research, i) => (
+                  <ResearchCard
+                    key={research.title}
+                    research={research}
+                    index={i + 3}
+                    scrollYProgress={scrollYProgress}
+                  />
+                ))}
+              </ul>
+            </div>
           </div>
-          <div className="grid grid-cols-2 gap-32">
-            <ul className="flex flex-col gap-32">
-              {researches.slice(0, 3).map((research, i) => (
-                <li
-                  key={research.title}
-                  className={`p-16 min-w-[192px] border border-gray-800 border-dashed flex flex-col gap-12`}
-                >
-                  <research.icon />
-                  <p className="text-body3 text-gray-800 uppercase font-normal">
-                    {research.title}
-                  </p>
-                  <p
-                    className={`text-body5 text-gray-800 ${serifText.className} italic leading-6`}
-                  >
-                    {research.description}
-                  </p>
-                </li>
-              ))}
-            </ul>
-            <ul className="flex flex-col gap-32 mt-64">
-              {researches.slice(3).map((research, i) => (
-                <li
-                  key={research.title}
-                  className={`p-16 min-w-[192px] border border-gray-800 border-dashed flex flex-col gap-12`}
-                >
-                  <research.icon />
-                  <p className="text-body3 text-gray-800 uppercase font-normal">
-                    {research.title}
-                  </p>
-                  <p
-                    className={`text-body5 text-gray-800 ${serifText.className} italic leading-6`}
-                  >
-                    {research.description}
-                  </p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </section>
+  );
+}
+
+function ResearchCard({
+  research,
+  scrollYProgress,
+  index,
+}: {
+  research: (typeof researches)[0];
+  scrollYProgress: MotionValue<number>;
+  index: number;
+}) {
+  const segmentLength = 1 / researches.length;
+  const segmentStart = segmentLength * index;
+  const segmentEnd = segmentStart + segmentLength;
+
+  const opacity = useTransform(
+    scrollYProgress,
+    [segmentStart, segmentEnd],
+    [0, 1]
+  );
+
+  const y = useTransform(scrollYProgress, [segmentStart, segmentEnd], [200, 0]);
+
+  return (
+    <motion.li
+      initial={{ opacity: 0, y: 200 }}
+      style={{ opacity, y }}
+      key={research.title}
+      className={`p-16 min-w-[192px] border border-gray-800 border-dashed flex flex-col gap-12`}
+    >
+      <research.icon />
+      <p className="text-body3 text-gray-800 uppercase font-normal">
+        {research.title}
+      </p>
+      <p
+        className={`text-body5 text-gray-800 ${serifText.className} italic leading-6`}
+      >
+        {research.description}
+      </p>
+    </motion.li>
   );
 }
 
