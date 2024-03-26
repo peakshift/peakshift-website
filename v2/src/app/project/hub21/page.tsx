593e137b8a06a54efb5d0289dcf3fed752ba03df
Johns Beharry <johns@peakshift.com>
2024-03-26 02:14:54 +0100

    fix(projects): escape entities

--- v2/src/app/project/hub21/page.tsx
+++ v2/src/app/project/hub21/page.tsx
@@ -59,10 +59,7 @@ export default function HUB21Page() {
         </section>
         <section className="grid grid-cols-1 md:grid-cols-3 gap-x-100 gap-y-24 py-40 relative">
           <div>
-            <Image
-              src={FirstPhotoImage}
-              alt="Photo of the space"
-            />
+            <Image src={FirstPhotoImage} alt="Photo of the space" />
             <Image
               src={CrossHairsImage}
               alt="Lines"
@@ -70,11 +67,26 @@ export default function HUB21Page() {
             />
           </div>
           <div className="col-span-2">
-            <p className={`${serifText.className} text-body4 md:text-body1 pb-24`}>
-              HUB21 is a non-profit association in Belgrade dedicated to education, training, and community-building within the global Bitcoin design and development ecosystem. Founded by 2 prominant local bitcoiners and Johns, the founder of PEAK SHIFT, HUB21 aims to provide a welcoming space for professionals and creatives from various fields to collaborate and innovate.
+            <p
+              className={`${serifText.className} text-body4 md:text-body1 pb-24`}
+            >
+              HUB21 is a non-profit association in Belgrade dedicated to
+              education, training, and community-building within the global
+              Bitcoin design and development ecosystem. Founded by 2 prominant
+              local bitcoiners and Johns, the founder of PEAK SHIFT, HUB21 aims
+              to provide a welcoming space for professionals and creatives from
+              various fields to collaborate and innovate.
             </p>
-            <p className={`${serifText.className} text-body4 md:text-body1 pb-2`}>
-              During the planning phase of HUB21, we encountered several unique challenges that required creative solutions. One significant challenge was the constraint of a limited budget, coupled with the nascent Bitcoin ecosystem in Belgrade. Unlike more established tech hubs, such as Silicon Valley, Belgrade primarily relies on outsourced tech talent, limiting the local entrepreneur landscape's innovation potential.
+            <p
+              className={`${serifText.className} text-body4 md:text-body1 pb-2`}
+            >
+              During the planning phase of HUB21, we encountered several unique
+              challenges that required creative solutions. One significant
+              challenge was the constraint of a limited budget, coupled with the
+              nascent Bitcoin ecosystem in Belgrade. Unlike more established
+              tech hubs, such as Silicon Valley, Belgrade primarily relies on
+              outsourced tech talent, limiting the local entrepreneur
+              landscape's innovation potential.
             </p>
             <div className="flex justify-end mt-24">
               <Image src={AsterixLogo} alt="Asterix Logo" />
@@ -111,31 +123,28 @@ export default function HUB21Page() {
             <h2
               className={`${condensedHeadings.className} text-h3 md:text-[48px] uppercase`}
             >
-              How Might We Establish HUB21 as the Premier Destination for Freedom Tech in Belgrade?
+              How Might We Establish HUB21 as the Premier Destination for
+              Freedom Tech in Belgrade?
             </h2>
             <p className="text-body4 md:text-body1">
-              To address the challenges of limited budget and the nascent Bitcoin ecosystem in Belgrade, the HUB21 team adopted a strategic approach focused on inclusivity and approachability in the branding. Recognizing the importance of creating an environment that appeals to individuals beyond the deeply technical, vibrant and youthful colors, such as bright green and yellow, coupled with a strong black for typography, were selected. This choice aimed to create a welcoming atmosphere for individuals from diverse backgrounds, regardless of their familiarity with Bitcoin.
+              To address the challenges of limited budget and the nascent
+              Bitcoin ecosystem in Belgrade, the HUB21 team adopted a strategic
+              approach focused on inclusivity and approachability in the
+              branding. Recognizing the importance of creating an environment
+              that appeals to individuals beyond the deeply technical, vibrant
+              and youthful colors, such as bright green and yellow, coupled with
+              a strong black for typography, were selected. This choice aimed to
+              create a welcoming atmosphere for individuals from diverse
+              backgrounds, regardless of their familiarity with Bitcoin.
             </p>
           </div>
         </section>
         <section className="py-40 md:py-80">
           <div className="flex justify-center">
             <div className="w-[120%] shrink-0 flex gap-24 ">
-              <Image
-                src={LogoWireframe}
-                alt="Wireframe"
-                className="min-w-0"
-              />
-              <Image
-                src={Logo}
-                alt="Logo"
-                className="min-w-0"
-              />
-              <Image
-                src={LogoWireframe}
-                alt="Wireframe"
-                className="min-w-0"
-              />
+              <Image src={LogoWireframe} alt="Wireframe" className="min-w-0" />
+              <Image src={Logo} alt="Logo" className="min-w-0" />
+              <Image src={LogoWireframe} alt="Wireframe" className="min-w-0" />
             </div>
           </div>
         </section>
@@ -159,7 +168,12 @@ export default function HUB21Page() {
               Biophilic Design Elements
             </h3>
             <p className="text-body4 md:text-body1">
-              Recognizing the positive impact of nature on well-being and productivity, HUB21 made deliberate efforts to incorporate biophilic design elements throughout the space, despite challenges with natural light. In an older building situated in the center of the city, accessibility was prioritized, but limitations in natural light penetration were addressed creatively.
+              Recognizing the positive impact of nature on well-being and
+              productivity, HUB21 made deliberate efforts to incorporate
+              biophilic design elements throughout the space, despite challenges
+              with natural light. In an older building situated in the center of
+              the city, accessibility was prioritized, but limitations in
+              natural light penetration were addressed creatively.
             </p>
           </div>
           <div
@@ -169,7 +183,13 @@ export default function HUB21Page() {
               Innovative Solutions in a Challenging Environment
             </h3>
             <p className="text-body4 md:text-body1">
-              Despite the limited natural light due to neighboring buildings obstructing windows on three sides of the main workspace, HUB21 implemented innovative solutions to create a welcoming and inspiring environment. Indoor plants were strategically placed to bring elements of nature indoors and improve air quality. Mirrors were strategically positioned to reflect available natural light, enhancing the perception of space and brightness.
+              Despite the limited natural light due to neighboring buildings
+              obstructing windows on three sides of the main workspace, HUB21
+              implemented innovative solutions to create a welcoming and
+              inspiring environment. Indoor plants were strategically placed to
+              bring elements of nature indoors and improve air quality. Mirrors
+              were strategically positioned to reflect available natural light,
+              enhancing the perception of space and brightness.
             </p>
           </div>
           <div
@@ -179,7 +199,12 @@ export default function HUB21Page() {
               Integration of Artificial Lighting
             </h3>
             <p className="text-body4 md:text-body1">
-              To compensate for the lack of natural light, HUB21 invested in high-quality artificial lighting solutions. LED lighting fixtures with adjustable brightness and color temperature were installed throughout the space to mimic natural daylight as closely as possible. Task lighting was also provided to ensure optimal visibility and comfort for members working on detailed tasks.
+              To compensate for the lack of natural light, HUB21 invested in
+              high-quality artificial lighting solutions. LED lighting fixtures
+              with adjustable brightness and color temperature were installed
+              throughout the space to mimic natural daylight as closely as
+              possible. Task lighting was also provided to ensure optimal
+              visibility and comfort for members working on detailed tasks.
             </p>
           </div>
           <div
@@ -189,7 +214,12 @@ export default function HUB21Page() {
               Materials Inspired by Nature
             </h3>
             <p className="text-body4 md:text-body1">
-              In addition to incorporating living elements such as plants, HUB21 selected materials and finishes inspired by nature to create a sense of connection to the outdoors. Earthy tones, natural wood accents, and textured surfaces were used to evoke a sense of warmth and tranquility, despite the limitations of the building's environment.
+              In addition to incorporating living elements such as plants, HUB21
+              selected materials and finishes inspired by nature to create a
+              sense of connection to the outdoors. Earthy tones, natural wood
+              accents, and textured surfaces were used to evoke a sense of
+              warmth and tranquility, despite the limitations of the building's
+              environment.
             </p>
           </div>
           <div
@@ -199,7 +229,13 @@ export default function HUB21Page() {
               Creating a Calming and Inspiring Environment
             </h3>
             <p className="text-body4 md:text-body1">
-              Despite the challenges posed by the building's location and limited natural light, HUB21 succeeded in creating a calming and inspiring environment for its members. By carefully integrating biophilic design elements, leveraging innovative lighting solutions, and selecting materials that evoke nature, HUB21 transformed its space into a welcoming oasis in the heart of the city, where creativity and productivity flourish.
+              Despite the challenges posed by the building's location and
+              limited natural light, HUB21 succeeded in creating a calming and
+              inspiring environment for its members. By carefully integrating
+              biophilic design elements, leveraging innovative lighting
+              solutions, and selecting materials that evoke nature, HUB21
+              transformed its space into a welcoming oasis in the heart of the
+              city, where creativity and productivity flourish.
             </p>
           </div>
         </section>
